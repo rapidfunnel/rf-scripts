66e70486a4780e27ab13e832f0e46cbe32e07515
rfdevrock <111121850+rfdevrock@users.noreply.github.com>
2024-10-03 15:44:09 +0530

    Update userDetails.js to use ID of elements instead of class

--- v1/userDetails.js
+++ v1/userDetails.js
@@ -6,6 +6,10 @@ jQuery(function ($) {
   const resourceId = params[2]; // Extract the resourceID
   const contactId = params[3]; // Extract the contactId
 
+  console.log('User ID: ' + userId);
+  console.log('Resource ID: ' + resourceId);
+  console.log('Contact ID: ' + contactId);
+
   if (userId) {
     $.get(
       'https://apiv2.rapidfunnel.com/v2/users-details/' + userId,
@@ -16,45 +20,64 @@ jQuery(function ($) {
         // Loop over the userData keys
         for (const key of Object.keys(userData)) {
           const value = userData[key];
-          const $element = $('.components--custom_' + key.toLowerCase());
+          const $element = $('#' + key);
 
-          // If it's not the customBookingLink, set the text for the element (replace the placeholder text)
-          if (key !== 'customBookingLink') {
-            $element.text(value);
-          }
-
-          if (key === 'profileImage') {
-            const imgSrc =
-              value !== ''
-                ? value
-                : 'https://rfres.com/assets/img/icon-user-default.png';
-            $element.attr('src', imgSrc);
-          } else if (key === 'email') {
-            $element.attr('href', 'mailto:' + value).text(value);
-          } else if (key === 'phoneNumber') {
-            if (value !== '') {
-              $element.attr('href', 'tel:' + value).text(value);
-            } else {
-              $element.parent().hide(); // Hide the parent if phoneNumber is empty
+          if ($element.length) {
+            // If it's not the customBookingLink, set the text for the element (replace the placeholder text)
+            if (key !== 'customBookingLink') {
+              $element.text(value);
             }
-          } else if (key === 'customBookingLink') {
-            if (value !== '') {
-              $element.attr('href', value);
-            } else {
-              $element.hide();
-            }
-          }
 
-          // Handle social links
-            $('.components--footer-social-links a').each(function () {
-              const socialId = $(this).attr('id'); // Get the id of the element (e.g., facebookUrl, twitterUrl)
-              
-              if (userData.hasOwnProperty(socialId) && userData[socialId].trim() !== '') {
-                $(this).attr('href', userData[socialId]); // Set href if value exists in userData
+            if (key === 'profileImage') {
+              const imgSrc =
+                value !== ''
+                  ? value
+                  : 'https://rfres.com/assets/img/icon-user-default.png';
+              $element.attr('src', imgSrc);
+            } else if (key === 'email') {
+              $element.attr('href', 'mailto:' + value).text(value);
+            } else if (key === 'phoneNumber') {
+              if (value !== '') {
+                $element.attr('href', 'tel:' + value).text(value);
               } else {
-                $(this).hide();
+                $element.parent().hide(); // Hide the parent if phoneNumber is empty
               }
-            });
+            } else if (key === 'customBookingLink') {
+              if (value !== '') {
+                $element.attr('href', value);
+              } else {
+                $element.hide();
+              }
+            }
+          }
+
+          // Handle social links (replace href if available, otherwise hide the element)
+          // $('.footer-social-links a').each(function () {
+          //   const socialKey = $(this).data('social');
+          //   if (
+          //     userData.hasOwnProperty(socialKey) &&
+          //     userData[socialKey].trim() !== ''
+          //   ) {
+          //     $(this)
+          //       .attr('href', userData[socialKey])
+          //       .text(userData[socialKey]); // Set href and text
+          //   } else {
+          //     $(this).hide(); // Hide if the social link is empty
+          //   }
+          // });
+          // Handle social links (replace href if available, otherwise hide the element)
+          $('.footer-social-links a').each(function () {
+            const socialId = $(this).attr('id'); // Get the id of the element (e.g., facebookUrl, twitterUrl)
+
+            if (
+              userData.hasOwnProperty(socialId) &&
+              userData[socialId].trim() !== ''
+            ) {
+              $(this).attr('href', userData[socialId]); // Set href if value exists in userData
+            } else {
+              $(this).hide(); // Hide the element if no value exists for the socialId
+            }
+          });
         }
       }
     ).fail(function () {
