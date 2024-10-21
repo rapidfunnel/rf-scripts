867ff511612226270372c3dc509afa4c4413e908
rfdevrock <111121850+rfdevrock@users.noreply.github.com>
2024-10-21 15:46:02 +0530

    Only show book me button if href value is not equal to #

--- v1/bookMe.js
+++ v1/bookMe.js
@@ -6,7 +6,7 @@ jQuery(function ($) {
     // Function to check if the button should be shown based on the href attribute
     function checkAndShowButton() {
         console.log("In Book me script, book me href value", $bookMebutton.attr('href'));
-        if ($bookMebutton.attr('href')) {
+        if ($bookMebutton.attr('href') && $bookMebutton.attr('href') !== '#') {
             $bookMebutton.show();
         } else {
             $bookMebutton.hide();
