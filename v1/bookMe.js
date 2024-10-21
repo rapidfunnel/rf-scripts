a7fbd6846a5a46a64e7c3cbcf4b5e1b90335f4f6
rfdevrock <111121850+rfdevrock@users.noreply.github.com>
2024-10-21 15:55:30 +0530

    Listen for custom event if book me btn href changes

--- v1/bookMe.js
+++ v1/bookMe.js
@@ -3,6 +3,11 @@ jQuery(function ($) {
     const $bookMebutton = $('#customBookingLink');
     $bookMebutton.hide();
 
+    // Listen for the custom event
+    $(document).on('customBookingLinkUpdated', function () {
+        checkAndShowButton();
+    });
+
     // Function to check if the button should be shown based on the href attribute
     function checkAndShowButton() {
         console.log("In Book me script, book me href value", $bookMebutton.attr('href'));
