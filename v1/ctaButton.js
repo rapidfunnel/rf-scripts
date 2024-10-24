85e54daa2aa7cd4a54d2c6a00c590dc2d5153e75
rfdevrock <111121850+rfdevrock@users.noreply.github.com>
2024-10-24 19:46:33 +0530

    Add API call to send CTA Button email

--- v1/ctaButton.js
+++ v1/ctaButton.js
@@ -6,5 +6,48 @@ jQuery(function ($) {
   const resourceId = parsedUrl.searchParams.get('resourceId');
   const contactId = parsedUrl.searchParams.get('contactId');
 
+  function handleCtaButtonClick(buttonId) {
+    const ctaButtonLocation = window[buttonId];
+
+    // Get contact details
+    if(contactId) {
+      $.get(
+      'https://apiv2.rapidfunnel.com/v2/contact-details/' + contactId,
+      function (response) {
+        const contactData = response.data;
+        // Make a POST request with contactData to send cta button email to user
+        $.ajax({
+          url: 'https://app.rapidfunnel.com/api/mail/send-ctaemail',
+          type: 'POST',
+          contentType: 'application/json',
+          dataType: "json",
+          data: {
+            legacyUserId: userId,
+            contactFirstName: contactData.firstName,
+            contactLastName: contactData.lastName,
+            contactPhoneNumber: contactData.phone,
+            contactEmail: contactData.email,
+            ctaLocation: ctaButtonLocation,
+            ctaPageName: pageName
+          },
+          success: function (response) {
+            console.log('CTA Button email sent successfully', response);
+          },
+          error: function (xhr, status, error) {
+            console.error('CTA Button email failed', error);
+          }
+        });
+      }
+      ).fail(function () {
+        console.error('Failed to fetch contact details.');
+      });
+    }
+  }
   
+
+  
+  $('[id^="ctaButton"]').on('click', function(event) {
+      event.preventDefault();
+      handleCtaButtonClick(this.id);
+  });
 });
