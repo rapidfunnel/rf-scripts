453e319e8d35493323a2996911eae4d352b97a02
rfdevrock <111121850+rfdevrock@users.noreply.github.com>
2024-11-27 13:14:14 +0100

    Update userDetails.js

--- v1/userDetails.js
+++ v1/userDetails.js
@@ -7,6 +7,7 @@ jQuery(function ($) {
     const contactId = parsedUrl.searchParams.get('contactId');
     const emailIcon = document.querySelector('span.fa.fa-envelope.fa-lg');
     const phoneIcon = document.querySelector('span.fa.fa-phone.fa-lg');
+    const socialIcons = document.querySelectorAll('span.fa-xl');
 
     console.log('User ID: ' + userId);
     console.log('Resource ID: ' + resourceId);
@@ -27,6 +28,27 @@ jQuery(function ($) {
         styleSheet.cssRules.length
       );
     }
+
+    if (socialIcons.length > 0) {
+      const styleSheet = document.styleSheets[0];
+      styleSheet.insertRule(
+        'span.fa-xl::before { content: ""; }',
+        styleSheet.cssRules.length
+      );
+    }
+    
+    socialIcons.forEach((icon) => {
+      const computedStyle = window.getComputedStyle(icon, "::before");
+      const content = computedStyle.getPropertyValue("content");
+  
+      if (content !== "") {
+        const styleSheet = document.styleSheets[0];
+        styleSheet.insertRule(
+          span.fa-xl::before { content: ""; },
+          styleSheet.cssRules.length
+        );
+      }
+    });
 
     if (userId) {
       $.get(
