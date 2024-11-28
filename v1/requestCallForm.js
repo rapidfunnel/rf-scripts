c2c60d993de0d6e49861ca7e067ca8fd2318a967
rfdevrock <111121850+rfdevrock@users.noreply.github.com>
2024-11-28 16:25:03 +0100

    Update requestCallForm.js

--- v1/requestCallForm.js
+++ v1/requestCallForm.js
@@ -20,41 +20,41 @@ jQuery(function ($) {
   }
 
   function requestCallButtonSubmit() {
-    // Called for known contact
-    if(contactId) {
-      $.get(
-      'https://apiv2.rapidfunnel.com/v2/contact-details/' + contactId,
-      function (response) {
-        const contactData = response.data;
-        // Make a POST request with contactData to send request call email to user
-        $.ajax({
-          url: 'https://app.rapidfunnel.com/api/mail/send-request-call-email',
-          type: 'POST',
-          contentType: 'application/json',
-          dataType: "json",
-          data: JSON.stringify({
-            legacyUserId: numericUserId,
-            contactFirstName: contactData.firstName,
-            contactLastName: contactData.lastName,
-            contactPhoneNumber: contactData.phone,
-            requestCallSourcePage: resourceDescriptionForRequestCall
-          }),
-          success: function (response) {
-            console.log('Request Call email sent successfully', response);
-          },
-          error: function (xhr, status, error) {
-            console.error('Request Call email failed', error);
-          }
-        });
-      }
-      ).fail(function () {
-        console.error('Failed to fetch contact details.');
-      });
-    }
+    console.log('check1111');
+    // // Called for known contact
+    // if(contactId) {
+    //   $.get(
+    //   'https://apiv2.rapidfunnel.com/v2/contact-details/' + contactId,
+    //   function (response) {
+    //     const contactData = response.data;
+    //     // Make a POST request with contactData to send request call email to user
+    //     $.ajax({
+    //       url: 'https://app.rapidfunnel.com/api/mail/send-request-call-email',
+    //       type: 'POST',
+    //       contentType: 'application/json',
+    //       dataType: "json",
+    //       data: JSON.stringify({
+    //         legacyUserId: numericUserId,
+    //         contactFirstName: contactData.firstName,
+    //         contactLastName: contactData.lastName,
+    //         contactPhoneNumber: contactData.phone,
+    //         requestCallSourcePage: resourceDescriptionForRequestCall
+    //       }),
+    //       success: function (response) {
+    //         console.log('Request Call email sent successfully', response);
+    //       },
+    //       error: function (xhr, status, error) {
+    //         console.error('Request Call email failed', error);
+    //       }
+    //     });
+    //   }
+    //   ).fail(function () {
+    //     console.error('Failed to fetch contact details.');
+    //   });
+    // }
   }
 
   function requestCallFormSubmit() {
-    console.log('check123');
     //   const contactFirstName = $('#contactFirstNameRequestForm').val();
     //   const contactLastName = $('#contactLastNameRequestForm').val();
     //   const contactPhoneNumber = $('#contactPhoneRequestForm').val();
