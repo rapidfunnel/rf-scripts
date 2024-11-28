e2330a72c19ffbef2e888b6c268d5b3e16cce3a9
rfdevrock <111121850+rfdevrock@users.noreply.github.com>
2024-11-28 11:13:50 +0100

    Update requestCallForm.js

--- v1/requestCallForm.js
+++ v1/requestCallForm.js
@@ -101,8 +101,8 @@ $('#requestForm form').on('submit', function (event) {
     }),
     success: function (response) {
       console.log('Request Call email sent successfully', response);
-      $('#requestForm .w-form-done').show(); 
-      $('#requestForm .w-form-fail').hide();
+      // $('#requestForm .w-form-done').show(); 
+      // $('#requestForm .w-form-fail').hide();
     },
     error: function (xhr, status, error) {
       console.error('Request Call email failed', error);
