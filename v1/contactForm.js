ad0af93875ca9960cfc05e651005264744afc1cd
Oefepe <167247978+Oefepe@users.noreply.github.com>
2025-01-16 14:58:02 -0300

    Update contactForm.js

--- v1/contactForm.js
+++ v1/contactForm.js
@@ -70,7 +70,7 @@ jQuery(function ($) {
           '&lastName=' + document.getElementById('contactLastName').value +
           '&email=' + document.getElementById('contactEmail').value +
           '&phone=' + document.getElementById('contactPhone').value +
-          '&campaign=' + assignCampaignId +
+          '&campaign=' + CampaignId +
           '&contactTag=' + labelId;
 
     // You can add city and state by appending the following to formData variable:
