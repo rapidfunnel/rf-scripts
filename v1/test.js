d1327334286830734b2ee8e2341bb0b92640a021
Oefepe <167247978+Oefepe@users.noreply.github.com>
2025-01-24 13:54:23 -0300

    Update test.js

--- v1/test.js
+++ v1/test.js
@@ -1,128 +1,151 @@
-<script>
 jQuery(function ($) {
-  // Parse the URL to extract userId, resourceID, and contactId
-  const parsedUrl = new URL(window.location.href);
-  // const params = parsedUrl.search.split('/');
-  // const userId = params[1]; // Extract the userId
-  // const resourceId = params[2]; // Extract the resourceID
-  // const contactId = params[3]; // Extract the contactId
-
-  const userId = parsedUrl.searchParams.get('userId');
-  const resourceId = parsedUrl.searchParams.get('resourceId');
-  const contactId = parsedUrl.searchParams.get('contactId');
-
-  console.log('User ID: ' + userId);
-  console.log('Resource ID: ' + resourceId);
-  console.log('Contact ID: ' + contactId);
-
-    // Capture and format the redirect URL immediately
-  let contactFormLink = $('#contactFormSubmitContainer').attr('data-redirect') || $('#contactFormSubmitContainer').data('redirect');
-  console.log('contactFormLink', contactFormLink);
-  console.log('contactFormLink redirdect', $('#contactFormSubmitContainer').attr('data-redirect'));
-  let originalContactFormLink = contactFormLink;
-  if (contactFormLink) {
-    // Format the redirect URL with the dynamic values
-    contactFormLink = contactFormLink
-      .replace('[user-id]', userId || '')
-      .replace('[resourceID]', resourceId || '')
-      .replace('[contactId]', contactId || '');
-
-    console.log('Formatted Redirect URL:', contactFormLink);
-
-    // Remove redirect attributes immediately to prevent Webflow's default behavior
-    // $('#contactFormSubmitBtn').removeAttr('redirect').removeAttr('data-redirect');
-  }
-
-  // If contactId exists, make an API call to get contact details
-  if (contactId) {
-    $('#contactEmail').prop('disabled', true);  // Disable the email input field
-    $('label[for="email"]').css('color', '#aaa');
-
-    $.get(
-      'https://apiv2.rapidfunnel.com/v2/contact-details/' + contactId,
-      function (response) {
-        const contactData = response.data;
-
-        // Populate the form with contact details
-        $('.contactfirstname').val(contactData.firstName);
-        $('.contactlastname').val(contactData.lastName);
-        $('.contactemail').val(contactData.email);
-        $('.contactphone').val(contactData.phone);
-        $('.contactnote').val(contactData.note);
-      }
-    ).fail(function () {
-      console.error('Failed to fetch contact details.');
-    });
-  } else {
-    console.log('No contactId found in the URL.');
-  }
-
-  function todayDate() {
-    const today = new Date();
-    return today.toISOString();
-  }
-
-  // Handle form submission
-  $('#contactFormSubmitBtn').on('click', function (event) {
-    event.preventDefault(); // Prevent the default form submission behavior
-    $('#contactFormSubmitBtn').attr('disabled', false)
-
-    var formData = 'firstName=' + document.getElementById('contactFirstName').value +
-          '&lastName=' + document.getElementById('contactLastName').value +
-          '&email=' + document.getElementById('contactEmail').value +
-          '&phone=' + document.getElementById('contactPhone').value +
-          '&campaign=' + campaignId +
-          '&contactTag=' + labelId;
-
-    // You can add city and state by appending the following to formData variable:
-    //      '&streetaddress=' + 'testStreet' +
-    //      '&city=' + 'Denver' +
-    //      '&pincode=' + '80401';
-
-
-    // Submit the form data to the API
-    $.ajax({
-      url: 'https://my.rapidfunnel.com/landing/resource/create-custom-contact',
-      method: 'POST',
-      dataType: "json",
-      data: {
-        formData: formData,
-        resourceId: resourceId,
-        senderId: userId,
-        sentFrom: 'customPage'
-      },
-      success: function (response) {
-        console.log(response);
-        if (response.contactId > 0) {
-          console.log('Form submitted successfully!');
-
-          contactFormLink = originalContactFormLink
-            .replace('[user-id]', userId || '')
-            .replace('[resourceID]', resourceId || '')
-            .replace('[contactId]', response.contactId || '');
-          console.log('Contact Form redirect URL with contactId', contactFormLink);
-
-          // Open linked URL
-          if (contactFormLink) {
-            window.location.href = contactFormLink;
-          }
-        } else {
-          alert('A contact could not be added!');
-          // Open linked URL
-          if (contactFormLink) {
-            contactFormLink = contactFormLink.replace(contactFormLink, "null");
-          }
+  
+    // Adding Wistia analytics tracking class to iframes
+    $("iframe").each(function() {
+        var str = $(this).attr('src');
+        if (str.indexOf("wistia") >= 0) {
+            $(this).addClass('wistia_embed').attr('name', 'wistia_embed');
         }
-      },
-      error: function (error) {
-        alert('Error submitting the form.');
-        console.error(error);
-        // Open linked URL
-          if (contactFormLink) {
-            contactFormLink = contactFormLink.replace(contactFormLink, "null");
-          }
-      },
     });
-  });
+    videoAnalytics();
+
+    // Listen for the custom event
+    $(document).on('customBookingLinkUpdated', function () {
+        checkBookingLink();
+    });
+
+    // Function to check the booking link and show/hide the request call container
+    function checkBookingLink() {
+        if ($('#customBookingLink').attr('href') && $('#customBookingLink').attr('href') !== '#') {
+            console.log("We should hide request call container since there's a booking link");
+            $('#requestCallContainer').hide();
+        } else {
+            console.log("We should show request call container since there's no booking link");
+            $('#requestCallContainer').show();
+        }
+    }
+
+    // Run the check initially in case the href value is already set on page load
+    checkBookingLink();
+  
+    // Wistia video analytics tracking setup
+    function videoAnalytics() {
+        console.log('Inside video analytics');
+        window._wq = window._wq || [];
+        _wq.push({
+            "_all": function(video) {
+                var contactId,
+                    userId,
+                    resourceId,
+                    url = window.location.href,
+                    webinar = $('#webinar').val();
+
+                const parsedUrl = new URL(url);
+                userId = parsedUrl.searchParams.get('userId');
+                resourceId = parsedUrl.searchParams.get('resourceId');
+                contactId = parsedUrl.searchParams.get('contactId');
+
+                console.log('timeInSeconds', showBookMeAfterTimeInSecondsPassedInVideo);
+                if (showBookMeAfterTimeInSecondsPassedInVideo > 0) {
+                    $('.bookMeContainer').hide(); // Changed to class selector
+                }
+
+                if ('' !== contactId &&
+                    $.isNumeric(resourceId) && $.isNumeric(userId)) {
+                    var analyticObject = {
+                        resourceId: resourceId,
+                        contactId: contactId,
+                        userId: userId,
+                        percentWatched: 0,
+                        mediaHash: '',
+                        duration: '',
+                        visitorKey: '',
+                        eventKey: '',
+                        asyncStats: true,
+                        delayProcess: 0,
+                        webinar: webinar
+                    };
+
+                    video.bind('play', function() {
+                        console.log("Percent Watched: ", video.percentWatched());
+                        analyticObject.percentWatched = video.percentWatched();
+                        analyticObject.mediaHash = video.hashedId();
+                        analyticObject.duration = video.duration();
+                        analyticObject.visitorKey = video.visitorKey();
+                        analyticObject.eventKey = video.eventKey();
+                        analyticObject.asyncStats = true;
+                        analyticObject.delayProcess = 1;
+                        sqsPushAnalytics(analyticObject);
+                        analyticObject.delayProcess = 0;
+                        console.log("Analytic Object: ", analyticObject);
+                    });
+
+                    video.bind('percentwatchedchanged', function (percent, lastPercent) {
+                        if (percent !== lastPercent) {
+                            console.log('Percent Watched: ', percent);
+                        }
+                    });
+
+                    video.bind("secondchange", function() {
+                        console.log("book me href value", $('#customBookingLink').attr('href'));
+                        if (video.secondsWatched() >= showBookMeAfterTimeInSecondsPassedInVideo) {
+                            console.log("We should now show book me container as time specified has passed");
+                            $('.bookMeContainer').show(); // Changed to class selector
+                        }
+                        if (video.secondsWatched() == 5) {
+                            console.log("You've watched 5 seconds of this video!");
+                        }
+                        if (video.secondsWatched() == 10) {
+                            console.log("You've watched 10 seconds of this video!");
+                            console.log('video script, customBookingLink: ', window.sharedData.customBookingLink);
+                        }
+                        if (video.secondsWatched() == 15) {
+                            console.log("You've watched 15 seconds of this video!");
+                        }
+                    });
+
+                    video.email($('#contactEmail').val());
+                }
+            }
+        });
+    }
+
+    // Function to push video analytics to a remote server
+    function sqsPushAnalytics(analyticObject) {
+        var postUrl = "https://my.rapidfunnel.com/landing/resource/push-to-sqs";
+        $.ajax({
+            type: "POST",
+            url: postUrl,
+            dataType: "json",
+            async: analyticObject.asyncStats,
+            data: {
+                resourceId: analyticObject.resourceId,
+                contactId: analyticObject.contactId,
+                userId: analyticObject.userId,
+                percentageWatched: analyticObject.percentWatched,
+                mediaHash: analyticObject.mediaHash,
+                duration: analyticObject.duration,
+                visitorKey: analyticObject.visitorKey,
+                eventKey: analyticObject.eventKey,
+                delayProcess: analyticObject.delayProcess,
+                webinar: analyticObject.webinar
+            },
+            success: function(response) {
+                if (!response) {
+                    bootbox.confirm('Some error occurred. Please reload your video once.', function(result) {
+                        if (result) {
+                            location.reload();
+                        }
+                    });
+                }
+            },
+            error: function() {
+                bootbox.confirm('Some error occurred. Please reload your video once.', function(result) {
+                    if (result) {
+                        location.reload();
+                    }
+                });
+            }
+        });
+    }
 });
-</script>
