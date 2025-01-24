78698a5a28ce336cbcb60d6847e4f7f7739894d4
Oefepe <167247978+Oefepe@users.noreply.github.com>
2025-01-24 14:52:47 -0300

    Update test.js

--- v1/test.js
+++ v1/test.js
@@ -1,178 +1,114 @@
 jQuery(function ($) {
-  
-  // document.addEventListener('DOMContentLoaded', function() {
-      // Adding Wistia analytics tracking class to iframes
-      $("iframe").each(function() {
-          var str = $(this).attr('src');
-          if (str.indexOf("wistia") >= 0) {
-              $(this).addClass('wistia_embed').attr('name', 'wistia_embed');
-          }
-      });
-      videoAnalytics();
-  // });
+    // Add Wistia analytics tracking class to iframes
+    function addWistiaTrackingClass() {
+        $("iframe").each(function () {
+            var src = $(this).attr('src');
+            if (src && src.includes("wistia")) {
+                $(this).addClass('wistia_embed').attr('name', 'wistia_embed');
+            }
+        });
+    }
 
-    // Listen for the custom event
-    $(document).on('customBookingLinkUpdated', function () {
-        checkBookingLink();
-    });
-
-   // Function to check the booking link and show/hide the request call container
+    // Check the booking link and show/hide the request call container
     function checkBookingLink() {
-        if ($('#customBookingLink').attr('href') && $('#customBookingLink').attr('href') !== '#') {
-            console.log("We should hide request call container since there's a booking link");
+        const bookingLink = $('#customBookingLink').attr('href');
+        if (bookingLink && bookingLink !== '#') {
+            console.log("Hiding request call container - Booking link exists");
             $('#requestCallContainer').hide();
         } else {
-            console.log("We should show request call container since there's no booking link");
+            console.log("Showing request call container - No booking link");
             $('#requestCallContainer').show();
         }
     }
 
-    // Run the check initially in case the href value is already set on page load
-    checkBookingLink();
-  
-  // Wistia video analytics tracking setup
-  function videoAnalytics() {
-      console.log('Inside video analystics');
-      window._wq = window._wq || [];
-      _wq.push({
-          "_all": function(video) {
-              var contactId,
-                  userId,
-                  resourceId,
-                  url = window.location.href,
-                  webinar = $('#webinar').val(),
-                  args;
-            
-              const parsedUrl = new URL(url);
-              userId = parsedUrl.searchParams.get('userId');
-              resourceId = parsedUrl.searchParams.get('resourceId');
-              contactId = parsedUrl.searchParams.get('contactId');
-
-              console.log('timeInSeconds', showBookMeAfterTimeInSecondsPassedInVideo);
-              if (showBookMeAfterTimeInSecondsPassedInVideo > 0) {
-                $('#bookMeContainer').hide();
-              }
-            
-              if ('' !== contactId &&
-                  $.isNumeric(resourceId) && $.isNumeric(userId)) {
-                  var analyticObject = {
-                      resourceId: resourceId,
-                      contactId: contactId,
-                      userId: userId,
-                      percentWatched: 0,
-                      mediaHash: '',
-                      duration: '',
-                      visitorKey: '',
-                      eventKey: '',
-                      asyncStats: true,
-                      delayProcess: 0,
-                      webinar: webinar
-                  };
-                  var percentWatched = 0;
-                  video.bind('play', function() {
-                      // Log percent watched to test if it's being tracked correctly
-                      console.log("Percent Watched: ", video.percentWatched());
+    // Wistia video analytics tracking setup
+    function videoAnalytics() {
+        console.log('Initializing video analytics');
+        window._wq = window._wq || [];
+        _wq.push({
+            "_all": function (video) {
+                const urlParams = new URLSearchParams(window.location.search);
+                const userId = urlParams.get('userId');
+                const resourceId = urlParams.get('resourceId');
+                const contactId = urlParams.get('contactId');
+                const webinar = $('#webinar').val();
+                
+                if (contactId && $.isNumeric(resourceId) && $.isNumeric(userId)) {
+                    const analyticObject = {
+                        resourceId,
+                        contactId,
+                        userId,
+                        percentWatched: 0,
+                        mediaHash: '',
+                        duration: '',
+                        visitorKey: '',
+                        eventKey: '',
+                        asyncStats: true,
+                        delayProcess: 0,
+                        webinar
+                    };
                     
-                      analyticObject.percentWatched = video.percentWatched();
-                      analyticObject.mediaHash = video.hashedId();
-                      analyticObject.duration = video.duration();
-                      analyticObject.visitorKey = video.visitorKey();
-                      analyticObject.eventKey = video.eventKey();
-                      analyticObject.asyncStats = true;
-                      analyticObject.delayProcess = 1;
-                      sqsPushAnalytics(analyticObject);
-                      analyticObject.delayProcess = 0;
-
-                      // Log the whole analyticObject to check all values
-                      console.log("Analytic Object: ", analyticObject);
-                  });
-
-                  // // Clear the interval when the video is paused or ended
-                  // video.bind('pause end', function() {
-                  //     clearInterval(watchInterval); // This stops the real-time logging when the video is not playing
-                  // });
-
-                  video.bind('percentwatchedchanged', function (percent, lastPercent) {
-                      if (percent !== lastPercent) {
-                        console.log('Percent Watched: ', percent);
-                      }
-                  });
-
-                  video.bind("secondchange", function() {
-    let secondsWatched = video.secondsWatched();
-    console.log("Seconds watched:", secondsWatched);
-    console.log("book me href value", $('#customBookingLink').attr('href'));
-
-    if (secondsWatched >= showBookMeAfterTimeInSecondsPassedInVideo) {
-        console.log("We should now show book me container as time specified has passed");
-        $('#bookMeContainer').show();
+                    video.bind('play', function () {
+                        console.log("Video started - Tracking analytics");
+                        analyticObject.percentWatched = video.percentWatched();
+                        analyticObject.mediaHash = video.hashedId();
+                        analyticObject.duration = video.duration();
+                        analyticObject.visitorKey = video.visitorKey();
+                        analyticObject.eventKey = video.eventKey();
+                        analyticObject.delayProcess = 1;
+                        sqsPushAnalytics(analyticObject);
+                    });
+                    
+                    video.bind('percentwatchedchanged', function (percent, lastPercent) {
+                        if (percent !== lastPercent) {
+                            console.log('Percent Watched:', percent);
+                        }
+                    });
+                    
+                    video.bind("secondchange", function () {
+                        const timeWatched = video.secondsWatched();
+                        console.log("Time watched: ", timeWatched);
+                        
+                        if (timeWatched >= showBookMeAfterTimeInSecondsPassedInVideo) {
+                            console.log("Displaying 'Book Me' button");
+                            $('#bookMeContainer').show();
+                        }
+                    });
+                    
+                    video.email($('#contactEmail').val());
+                }
+            }
+        });
     }
 
-    if (secondsWatched >= 5 && secondsWatched < 6) {
-        console.log("You've watched at least 5 seconds!");
+    // Function to push video analytics data
+    function sqsPushAnalytics(analyticObject) {
+        $.ajax({
+            type: "POST",
+            url: "https://my.rapidfunnel.com/landing/resource/push-to-sqs",
+            dataType: "json",
+            async: analyticObject.asyncStats,
+            data: analyticObject,
+            success: function (response) {
+                if (!response) {
+                    bootbox.confirm('Error occurred. Reload video?', function (result) {
+                        if (result) location.reload();
+                    });
+                }
+            },
+            error: function () {
+                bootbox.confirm('Error occurred. Reload video?', function (result) {
+                    if (result) location.reload();
+                });
+            }
+        });
     }
 
-    if (secondsWatched >= 10 && secondsWatched < 11) {
-        console.log("You've watched at least 10 seconds!");
-        if (window.sharedData && window.sharedData.customBookingLink) {
-            console.log('video script, customBookingLink: ', window.sharedData.customBookingLink);
-            $('#customBookingLink').show();
-        }
-    }
-
-    if (secondsWatched >= 15 && secondsWatched < 16) {
-        console.log("You've watched at least 15 seconds!");
-    }
-});
-
-// Ensure contact email exists before setting it
-let email = $('#contactEmail').val();
-if (email) {
-    video.email(email);
-} else {
-    console.warn("No contact email found!");
-}
-
-          }
-      });
-  }
-  
-  // Function to push video analytics to a remote server
-  function sqsPushAnalytics(analyticObject) {
-      var postUrl = "https://my.rapidfunnel.com/landing/resource/push-to-sqs";
-      $.ajax({
-          type: "POST",
-          url: postUrl,
-          dataType: "json",
-          async: analyticObject.asyncStats,
-          data: {
-              resourceId: analyticObject.resourceId,
-              contactId: analyticObject.contactId,
-              userId: analyticObject.userId,
-              percentageWatched: analyticObject.percentWatched,
-              mediaHash: analyticObject.mediaHash,
-              duration: analyticObject.duration,
-              visitorKey: analyticObject.visitorKey,
-              eventKey: analyticObject.eventKey,
-              delayProcess: analyticObject.delayProcess,
-              webinar: analyticObject.webinar
-          },
-          success: function(response) {
-              if (!response) {
-                  bootbox.confirm('Some error occurred. Please reload your video once.', function(result) {
-                      if (result) {
-                          location.reload();
-                      }
-                  });
-              }
-          },
-          error: function() {
-              bootbox.confirm('Some error occurred. Please reload your video once.', function(result) {
-                  if (result) {
-                      location.reload();
-                  }
-              });
-          }
-      })
-  }
+    // Event listeners
+    $(document).on('customBookingLinkUpdated', checkBookingLink);
+    
+    // Initial execution
+    addWistiaTrackingClass();
+    checkBookingLink();
+    videoAnalytics();
 });
